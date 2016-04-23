72d0b82388febdfc16415e2028071078506c95c6
jefk <speedracer111@gmail.com>
2016-04-22 19:14:26 -0700

    wip

--- src/test_dat_file_reader.py
+++ src/test_dat_file_reader.py
@@ -1,6 +1,10 @@
 import unittest
 
 class DatFileReaderTestCase(unittest.TestCase):
+    def setUp(self):
+        self.dat_file_reader = DatFileReader('test_file.dat')
 
-  def test_observations(self):
-      self.assertEquals(1, 1)
+    def test_observations(self):
+        expected = [{'day': 1, 'magnitude': 12},{'day': 10, 'magnitude': 42}]
+        observations = list(self.dat_file_reader.observations())
+        self.assertEquals(observations, expected)
