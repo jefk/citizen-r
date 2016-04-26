ebe1fe81794acbcf34df525fc62034d162442cc5
jefk <speedracer111@gmail.com>
2016-04-26 10:32:25 -0700

    dont need mock alias

--- src/test_dat_file_reader.py
+++ src/test_dat_file_reader.py
@@ -10,7 +10,7 @@ class DatFileReaderTestCase(unittest.TestCase):
     def test_observations(self):
         expected = [{'day': 1.0, 'magnitude': 12.0},{'day': 10.0, 'magnitude': 42.0}]
 
-        with mock.patch('__builtin__.open', self._mock_file(), create=True) as m:
+        with mock.patch('__builtin__.open', self._mock_file(), create=True):
             observations = list(self.dat_file_reader.observations())
 
         self.assertEquals(observations, expected)
